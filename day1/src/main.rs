df0c3dbd86e7fe50e5b9f120e69beb6d385ea505
valrus <imccowan@gmail.com>
2020-12-01 22:33:36 -0800

    Brute force part 2 also!!

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,6 +1,30 @@
 use std::env;
 use std::fs;
 
+fn print_pairs(numbers: &Vec<i32>) {
+    for (i, number1) in numbers.iter().enumerate() {
+        for number2 in &numbers[i..] {
+            if number1 + number2 == 2020 {
+                println!("{} + {} = 2020", number1, number2);
+                println!("{} * {} = {}", number1, number2, number1 * number2);
+            }
+        }
+    }
+}
+
+fn print_triples(numbers: &Vec<i32>) {
+    for (i, number1) in numbers.iter().enumerate() {
+        for (j, number2) in numbers[i..].iter().enumerate() {
+            for number3 in &numbers[i+j..] {
+                if number1 + number2 + number3 == 2020 {
+                    println!("{} + {} + {} = 2020", number1, number2, number3);
+                    println!("{} * {} * {} = {}", number1, number2, number3, number1 * number2 * number3);
+                }
+            }
+        }
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -15,12 +39,6 @@ fn main() {
         .map(|result| result.expect("Not a number"))
         .collect();
 
-    for (i, number1) in numbers.iter().enumerate() {
-        for number2 in &numbers[i..] {
-            if number1 + number2 == 2020 {
-                println!("{} + {} = 2020", number1, number2);
-                println!("{} * {} = {}", number1, number2, number1 * number2);
-            }
-        }
-    }
+    print_pairs(&numbers);
+    print_triples(&numbers);
 }
